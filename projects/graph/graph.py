6c3ada79ee6050389ce63594aa738b3a44afd724
stevekutz <stkutz@gmail.com>
2020-08-18 21:10:49 -0500

    completed parts 1,2,3

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -27,15 +27,40 @@ class Graph:
     # both vertices have to exist to make connection(e.g. directed edge)
 
         if v1 in self.vertices and v2 in self.vertices:
-            print(f' type(vertices) is {type(self.vertices)}')
+            # print(f' type(vertices) is {type(self.vertices)}')
             self.vertices[v1].add(v2)  # using set .add() method to append
         else:
-            print(f'ERROR: vertex {v1} or {v2} does not exist')     
+            # print(f'ERROR: vertex {v1} or {v2} does not exist')     
+            raise ValueError("Vertex not yet created")
+            # print(f'ERROR: vertex {v1} or {v2} does not exist')
+
+        ####   not quite
+        # try:
+        #     if v1 in self.vertices or v2 in self.vertices:
+        #         self.vertices[v1].add(v2)
+        # except:
+        #     raise ValueError(" BAD VERTEX !!")
+
+
+        if v1 not in self.vertices or v2 not in self.vertices:
+            raise ValueError(" BAD VERTEX !!")
+        else:
+            self.vertices[v1].add(v2)
+
+
+    def add_undirected_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)    
+
+        
 
     def show_connected(self):
+        print(f' Immediate neighbors')
         for item in self.vertices:
             for set_item in self.vertices[item]:
-                print(f'  dict {item}  contains set items: {self.vertices[item]}')
+                # print(f'  dict {item}  contains set items: {self.vertices[item]}')
+                print(f'  Vertex {item}  has edge to vertice(s) : {self.vertices[item]}')
                 break
 
 
@@ -48,20 +73,81 @@ class Graph:
             # print(f' self.vertices[vertex_id] >> {self.vertices[vertex_id]} ')
             return self.vertices[vertex_id]
         else:
-            print(f' ERROR:  vertex {vertex.id} does not exist')    
+            # print(f' ERROR:  vertex {vertex.id} does not exist')    
+            raise ValueError("get_neighbor cannot locate vertex {vertex_id}")
 
+
+    # this is a traversal, NOT SEARCH            
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        """      FIFO   ir LILO
+        Create a queue
+        Enqueue starting Vertex
+        Create a set top store visited
+        While the queue is NOT empty:  e.g.   > 0
+            Dequeue the first Vertex
+            Check IF NOT visited:
+                Mark as visited
+                enqueue ALL of neighbors
+        """
+
+        q = Queue()                      # create a queue  ( e.g. empty []  )
+        q.enqueue(starting_vertex)       # Enqeue starting at vertex
+        visited = set()                  # Create a set to store visited
+
+        # print(f' SIZE: {q.size()} ')
+        while q.size() > 0:              # While the queue is NOT empty:
+        # while q:             # ERROR: Will add  None  into v, breaks _get_neighbors
+            v = q.dequeue()                  # dequeue the first vertex
+            # print(f' dequeue v is : {v}')
+            if v not in visited:         # Check IF NOT visited:   
+                print(v)
+                visited.add(v)
+                # print(f' visited set is {visited} ')  
+                # print(f' v is:  {v}')         # if NOT visited, add to visited set
+                for n in self.get_neighbors(v):    # loop through all neighbors of  v 
+                    q.enqueue(n)                  # enqueue ALL neighbors found (ex. add to end of queue)
+                    # print(f' after enqueue of {n} \n')                      
+                    # print(f' after enqueue  visited set is {visited} ') 
+
+        # print(f' v is {v} ')
+        #return visited
+
+        # pass  # TODO
 
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
+        """       LIFO
+        Create a stack  
+        Push starting Vertex
+        Create a set to store visited
+        While the stack is NOT empty:    e.g.   > 0
+            Pop the last added Vertex
+            Check IF NOT visited:
+                Mark as visited
+                Push ALL of neighbors
+        """
+        s = Stack()              # Create a stack
+        s.push(starting_vertex)  # Push starting Vertex
+        visited = set()          # Create a set to store visited
+
+        while s.size() > 0:        #  While the stack is NOT empty:    e.g.   > 0
+            v = s.pop()          # Pop the last added Vertex
+            if v not in visited:
+                print(v)
+                visited.add(v)
+
+                for n in self.get_neighbors(v):   # Check IF NOT visited:
+                    s.push(n)                    # Push ALL of neighbors   
+
+        return visited
+
         pass  # TODO
 
     def dft_recursive(self, starting_vertex):
@@ -71,6 +157,17 @@ class Graph:
 
         This should be done using recursion.
         """
+        """
+        Check if Vertex is in visited
+            if NOT visited, add to visited set
+            Call dft_recursive on every neighbor    
+            
+
+        """
+        # 1) base case >> where to stop recursion
+        # 2) calls itself from within
+        # 3) each iteration approaches base case
+
         pass  # TODO
 
     def bfs(self, starting_vertex, destination_vertex):
@@ -124,25 +221,36 @@ if __name__ == '__main__':
     Should print:
         {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
     '''
-    print(graph.vertices)   # {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
-    graph.show_connected()
-        # dict 1  contains set items: {2}
-        # dict 2  contains set items: {3, 4}
-        # dict 3  contains set items: {5}
-        # dict 4  contains set items: {6, 7}
-        # dict 5  contains set items: {3}
-        # dict 6  contains set items: {3}
-        # dict 7  contains set items: {1, 6}
+    # print(graph.vertices)   # {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
+    # graph.show_connected()
+    #     #  Immediate neighbors
+    #     #   Vertex 1  had edge to vertice(s) : {2}
+    #     #   Vertex 2  had edge to vertice(s) : {3, 4}
+    #     #   Vertex 3  had edge to vertice(s) : {5}
+    #     #   Vertex 4  had edge to vertice(s) : {6, 7}
+    #     #   Vertex 5  had edge to vertice(s) : {3}
+    #     #   Vertex 6  had edge to vertice(s) : {3}
+    #     #   Vertex 7  had edge to vertice(s) : {1, 6}
 
+    # print(graph.get_neighbors(1))   # {2}
+    # print(graph.get_neighbors(4))   # {6,7}
 
+    # should raise Error
+    # graph.add_edge(1, 200)
 
+    graph.bft(1)
+    # print(f' bft   {graph.bft(1)} ')   # {1, 2, 3, 4, 5, 6, 7}
+    # print(f' dft   {graph.dft(1)} ')   # {1, 2, 3, 4, 5, 6, 7}
+
+    # print(f' bft   {graph.bft(2)} ')   # {1, 2, 3, 4, 5, 6, 7}
+    # print(f' dft   {graph.dft(2)} ')   # {1, 2, 3, 4, 5, 6, 7}
 
     # '''
     # Valid BFT paths:
     #     1, 2, 3, 4, 5, 6, 7
     #     1, 2, 3, 4, 5, 7, 6
     #     1, 2, 3, 4, 6, 7, 5
-    #     1, 2, 3, 4, 6, 5, 7
+    #     1, 2, 3, 4, 6, 5, 7    
     #     1, 2, 3, 4, 7, 6, 5
     #     1, 2, 3, 4, 7, 5, 6
     #     1, 2, 4, 3, 5, 6, 7
--- projects/graph/util.py
+++ projects/graph/util.py
@@ -4,10 +4,10 @@ class Queue():   # FIFO   or LILO
     def __init__(self):
         self.queue = []
     def enqueue(self, value):
-        self.queue.append(value)
+        self.queue.append(value)      # REMEBER periodic resizing
     def dequeue(self):
         if self.size() > 0:
-            return self.queue.pop(0)
+            return self.queue.pop(0)    # this is O(n)
         else:
             return None
     def size(self):
@@ -17,10 +17,10 @@ class Stack():      # LIFO
     def __init__(self):
         self.stack = []
     def push(self, value):
-        self.stack.append(value)
+        self.stack.append(value)        # REMEBER periodic resizing
     def pop(self):
         if self.size() > 0:
-            return self.stack.pop()
+            return self.stack.pop()     # this is O(n)
         else:
             return None
     def size(self):
@@ -28,21 +28,21 @@ class Stack():      # LIFO
 
 
 # q = Queue()
-# q.enqueue("A")
-# q.enqueue("B")
-# print(q.size())
+# q.enqueue("A")   # add "A" to end of queue, "like lining up to pay at store"
+# q.enqueue("B")   # add " B" to end of queue, "get in end of line"
+# print(q.size())  # 2
 # print(f' q is : {q.queue}')  # q is : ['A', 'B']
-# q.dequeue()
+# q.dequeue()    # FIFO, remove FIRST from queue and return it
 # print(f' q is {q.queue}')    # q is ['B']      FIFO
 
 
 
 # s = Stack()
-# s.push("First")
+# s.push("First")   # append to end of Stack
 # s.push("Next")
 # s.push("Last")
 # print(f' s is : {s.stack}')   # s is : ['First', 'Next', 'Last']
 # print(s.size())    # 3
-# s.pop()
+# s.pop()        # LIFO, remove last one added(ex. pushed) from Stack and return it
 # print(f' s is {s.stack}')    # s is ['First', 'Next']
 # print(s.size())    # 2
